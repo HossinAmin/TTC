a8f5031ff35725b0b7cbb382b1662a42bfdb076d
HossinAmin <hossinamin70@gmail.com>
2024-11-30 19:48:43 +0200

    move db file outside working directory

--- plugins/db.ts
+++ plugins/db.ts
@@ -2,8 +2,7 @@ import Database from "@tauri-apps/plugin-sql";
 import { resolve } from "@tauri-apps/api/path";
 
 export default defineNuxtPlugin(async () => {
-  const dbPath = await resolve("..", "my_db.db");
+  const dbPath = await resolve("../../DBs/TTC", "my_db.db");
   const db = await Database.load(`sqlite:${dbPath}`);
-
   return { provide: { db } };
 });
