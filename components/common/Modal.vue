9f73c62acc975d7cfc1c95dd9179afe77978ab77
HossinAmin <hossinamin70@gmail.com>
2024-11-30 12:04:00 +0200

    create common modal

---
+++ components/common/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    v-if="isOpen"
+    class="fixed top-0 flex h-full w-full items-center justify-center bg-surface/80"
+  >
+    <div
+      class="min-w-[50%] rounded-xl border border-solid border-footer bg-surface p-5"
+    >
+      <div class="flex justify-end">
+        <Icon
+          class="h-7 w-7 cursor-pointer active:text-footer"
+          name="ic:round-close"
+          @click="isOpen = false"
+        />
+      </div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isOpen = defineModel("isOpen", { default: false });
+</script>
