20b5c7eec6bf43d26cd4f639b09b50942698767b
HossinAmin <hossinamin70@gmail.com>
2024-11-30 16:11:42 +0200

    extract project car in separate file

---
+++ components/Project/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card-shadow flex flex-col gap-2 rounded-2xl bg-surface p-3">
+    <div class="flex justify-between">
+      <p class="text-lg font-semibold">{{ project.name }}</p>
+      <div class="flex gap-4">
+        <Icon
+          class="cursor-pointer text-xl active:text-footer"
+          name="ic:round-mode-edit-outline"
+          @click.stop="$emit('edit', project.id)"
+        />
+        <Icon
+          class="cursor-pointer text-xl active:text-footer"
+          name="ic:round-delete-forever"
+          @click="$emit('delete', project.id)"
+        />
+      </div>
+    </div>
+
+    <NuxtLink to="/projects/[id]">
+      <p class="text-sub">
+        {{ project.description }}
+      </p>
+
+      <div class="flex justify-between">
+        <p># tasks 5</p>
+        <p>time 00:00:00</p>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Project } from "~/types/Project";
+
+defineEmits<{
+  (e: "edit", id: string): void;
+  (e: "delete", id: string): void;
+}>();
+
+defineProps<{
+  project: Project;
+}>();
+</script>
