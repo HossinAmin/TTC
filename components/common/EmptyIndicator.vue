c8adf18e2a2725f8b1ad8ac81575888305926b10
HossinAmin <hossinamin70@gmail.com>
2024-12-07 00:24:24 +0200

    create empty indicator

---
+++ components/common/EmptyIndicator.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-10">
+    <img class="aspect-square h-[280px]" src="/assets/icons/empty.svg" />
+    <p class="text-lg font-semibold uppercase">{{ itemName }} list is empty</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ itemName: string }>();
+</script>
