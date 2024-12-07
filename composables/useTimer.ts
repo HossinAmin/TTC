335ef0ca1894ec267ea865e8a86ee8059e731b2c
HossinAmin <hossinamin70@gmail.com>
2024-12-07 14:02:21 +0200

    create timer composable

---
+++ composables/useTimer.ts
@@ -0,0 +1,43 @@
+export default function useTimer(initValue?: number) {
+  const seconds = ref(initValue ?? 0);
+  const play = ref(false);
+
+  let timerId: NodeJS.Timeout;
+
+  const time = computed(() => {
+    const h = Math.floor(seconds.value / 3600);
+    const m = Math.floor(seconds.value / 60) - h * 60;
+    const s = Math.floor(seconds.value - h * 3600 - m * 60);
+
+    return { h, m, s };
+  });
+
+  const pad = (num: number) => {
+    if (num < 10) {
+      return `0${num}`;
+    } else {
+      return `${num}`;
+    }
+  };
+
+  watch(play, () => {
+    if (play.value) {
+      timerId = setInterval(() => {
+        seconds.value += 1;
+      }, 1000);
+    } else if (!play.value && timerId) {
+      clearInterval(timerId);
+    }
+  });
+
+  onUnmounted(() => {
+    clearInterval(timerId);
+  });
+
+  return {
+    seconds,
+    play,
+    time,
+    pad,
+  };
+}
