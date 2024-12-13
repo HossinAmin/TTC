d05fdb36a8530c2cf15b1f67f81b62b18b87353b
HossinAmin <hossinamin70@gmail.com>
2024-12-13 19:37:25 +0200

    create check mouse coords command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,11 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_sql::Builder::new().build())
+        .invoke_handler(tauri::generate_handler![check_mouse_coords])
         .setup(|app| {
             #[cfg(desktop)]
             {
@@ -26,4 +28,24 @@ pub fn run() {
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+use device_query::{DeviceQuery, DeviceState};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct MouseCoords {
+    x: i32,
+    y: i32,
+}
+
+#[tauri::command]
+fn check_mouse_coords() -> MouseCoords {
+    let device_state = DeviceState::new();
+    let mouse_state = device_state.get_mouse();
+
+    MouseCoords {
+        x: mouse_state.coords.0,
+        y: mouse_state.coords.1,
+    }
 }
