d7c055a944b69fdb3d27042e167f7bf5c35bbe2f
HossinAmin <hossinamin70@gmail.com>
2024-12-13 18:41:25 +0200

    create system tray icon

---
+++ plugins/SystemTray.ts
@@ -0,0 +1,24 @@
+import { TrayIcon, type TrayIconOptions } from "@tauri-apps/api/tray";
+import { defaultWindowIcon } from "@tauri-apps/api/app";
+import { Menu } from "@tauri-apps/api/menu";
+
+export default defineNuxtPlugin(async () => {
+  const menu = await Menu.new({
+    items: [
+      {
+        id: "quit",
+        text: "Quit",
+      },
+    ],
+  });
+
+  const icon = await defaultWindowIcon();
+
+  const options = {
+    menu,
+    icon,
+    menuOnLeftClick: true,
+  };
+
+  const tray = await TrayIcon.new(options);
+});
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,20 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_sql::Builder::new().build())
         .setup(|app| {
+            #[cfg(desktop)]
+            {
+                tauri::tray::TrayIconBuilder::new()
+                    .on_tray_icon_event(|tray_handle, event| {
+                        // Use the method from the plugin
+                        tauri_plugin_positioner::on_tray_event(tray_handle.app_handle(), &event);
+                    })
+                    .build(app)?;
+            }
+
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
