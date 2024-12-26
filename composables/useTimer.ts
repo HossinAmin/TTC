94209ba4f4fe6a1b86d49e0d9f3a52b24b45bf4f
HossinAmin <hossinamin70@gmail.com>
2024-12-26 23:40:19 +0200

    chore: create time object type

--- composables/useTimer.ts
+++ composables/useTimer.ts
@@ -1,10 +1,12 @@
+import type { TimeObj } from "~/types/Time";
+
 export default function useTimer(initValue?: number) {
   const seconds = ref(initValue ?? 0);
   const play = ref(false);
 
   let timerId: NodeJS.Timeout;
 
-  const time = computed(() => {
+  const time = computed((): TimeObj => {
     const h = Math.floor(seconds.value / 3600);
     const m = Math.floor(seconds.value / 60) - h * 60;
     const s = Math.floor(seconds.value - h * 3600 - m * 60);
---
+++ types/Time.d.ts
@@ -0,0 +1,5 @@
+export type TimeObj = {
+  h: number;
+  m: number;
+  s: number;
+};
