18960baff359488b069fcae704388a0ed407629b
HossinAmin <hossinamin70@gmail.com>
2024-12-27 14:09:54 +0200

    feat: create notifications tray

---
+++ components/Toasts/Tray.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="fixed left-0 top-0 m-4 flex flex-col gap-4">
+    <TransitionGroup name="toast">
+      <div
+        v-for="toast in toasts"
+        class="flex flex-col gap-2 rounded-lg bg-surface p-3"
+        :key="toast.id"
+      >
+        <div v-if="toast?.title" class="flex items-center gap-2">
+          <Icon name="ic:outline-info" :size="24" />
+          <p>{{ toast.title }}</p>
+        </div>
+        <p class="text-sm text-sub">
+          {{ toast?.description }}
+        </p>
+      </div>
+    </TransitionGroup>
+  </div>
+</template>
+
+<script setup lang="ts">
+import useToast from "~/composables/useToast";
+
+const { toasts } = useToast();
+</script>
+
+<style scoped>
+.toast-enter-active,
+.toast-leave-active {
+  transition: all 0.5s ease;
+}
+.toast-enter-from,
+.toast-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+</style>
