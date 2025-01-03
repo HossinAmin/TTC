d28ecb1d5d78d3355ebe02cb009d08941e508d57
HossinAmin <hossinamin70@gmail.com>
2025-01-03 21:31:57 +0200

    hotfix: move images to assets resource

--- plugins/systemTray.ts
+++ plugins/systemTray.ts
@@ -4,7 +4,20 @@ import { Image } from "@tauri-apps/api/image";
 import { invoke } from "@tauri-apps/api/core";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 
+import { resolveResource } from "@tauri-apps/api/path";
+
+const getImgResource = async (path: string) => {
+  try {
+    const resource = await resolveResource(path);
+    return await Image.fromPath(resource);
+  } catch (err) {
+    throw new Error(`Failed to get resource with path: '${path}'`);
+  }
+};
+
 export default defineNuxtPlugin(async () => {
+  const appLogo = await getImgResource("assets/images/app_logo.png");
+
   const tray_id = await invoke<string>("get_tray_id");
   const tray = await TrayIcon.getById(tray_id);
   const menu = await Menu.new({
@@ -27,26 +40,25 @@ export default defineNuxtPlugin(async () => {
     ],
   });
 
-  const originalImage = await Image.fromPath("../assets/images/app_logo.png");
   tray?.setMenu(menu);
   tray?.setMenuOnLeftClick(false);
-  tray?.setIcon(originalImage);
+  tray?.setIcon(appLogo);
 
   /** Changes system tray icon to show that there is notification */
   const setTrayIconNotify = async () => {
-    const image = await Image.fromPath("../assets/images/notify_logo.png");
-    tray?.setIcon(image);
+    const img = await getImgResource("assets/images/notify_logo.png");
+    tray?.setIcon(img);
   };
 
   /** Changes system tray icon to show that there is notification */
   const setTrayIconPlay = async () => {
-    const image = await Image.fromPath("../assets/images/play_logo.png");
-    tray?.setIcon(image);
+    const img = await getImgResource("assets/images/play_logo.png");
+    tray?.setIcon(img);
   };
 
   /** Changes system tray icon back to default */
   const resetTrayIcon = async () => {
-    tray?.setIcon(originalImage);
+    tray?.setIcon(appLogo);
   };
 
   return {
