f7e8962352028ec6f2a03f984905c4d2ffd00554
HossinAmin <hossinamin70@gmail.com>
2025-01-13 21:15:08 +0200

    feat: create sidebar

---
+++ components/common/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <Transition name="drawer">
+    <div
+      v-if="isOpen"
+      class="flex min-h-[100%] min-w-[50%] flex-col bg-surface shadow-xl md:min-w-[30%]"
+    >
+      <slot />
+    </div>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+defineEmits(["close"]);
+const isOpen = defineModel("isOpen");
+</script>
+
+<style scoped>
+.drawer-enter-active,
+.drawer-leave-active {
+  transition: all 200ms ease-in-out;
+}
+
+.drawer-enter-from,
+.drawer-leave-to {
+  transform: translateX(-100%);
+}
+</style>
