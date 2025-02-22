b55e538921b0f1283940dce1c4d74a24644c0baa
HossinAmin <hossinamin70@gmail.com>
2025-02-22 16:03:24 +0200

    chore: truncate project name in sidebar

--- components/Project/Card.vue
+++ components/Project/Card.vue
@@ -8,22 +8,24 @@
       path: `/projects/${project.id}`,
     }"
   >
-    <div class="flex items-center gap-3">
+    <div class="flex flex-1 items-center gap-3 overflow-hidden">
       <Icon
+        class="min-h-8 min-w-8"
+        size="32px"
         :name="
           selected
             ? 'material-symbols:folder-open-rounded'
             : 'material-symbols:folder-rounded'
         "
-        :size="32"
       />
-      <div class="flex flex-col">
-        <p>{{ project.name }}</p>
-
-        <!-- truncate this -->
-        <p class="text-xs text-text-lighter">{{ project.description }}</p>
+      <div class="flex flex-col overflow-hidden">
+        <p class="truncate text-ellipsis">{{ project.name }}</p>
+        <p class="truncate text-ellipsis text-xs text-text-lighter">
+          {{ project.description }}
+        </p>
       </div>
     </div>
+
     <p
       class="flex aspect-square h-5 w-5 items-center justify-center rounded-full bg-text/20 text-xs text-text"
     >
