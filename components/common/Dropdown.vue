52adcf202838ab175408d5650a2b2adb28ad829c
HossinAmin <hossinamin70@gmail.com>
2025-02-22 21:21:12 +0200

    fix: drop down stays open after selection

--- components/common/Dropdown.vue
+++ components/common/Dropdown.vue
@@ -1,9 +1,5 @@
 <template>
-  <button
-    class="relative w-full"
-    @click="toggleDropdown"
-    @blur="isOpen = false"
-  >
+  <button class="relative w-full" ref="dropdown" @click="toggleDropdown">
     <p
       class="flex w-full items-center justify-between disabled:cursor-not-allowed disabled:bg-neutral-200"
     >
@@ -21,13 +17,13 @@
       leave-to-class="transform scale-95 opacity-0"
     >
       <ul
-        v-show="isOpen"
+        v-if="isOpen"
         class="absolute -left-20 z-10 my-1 max-h-60 w-28 overflow-auto rounded-lg bg-surface shadow-lg"
       >
         <li
           v-for="option in options"
           class="item flex w-full cursor-pointer items-center gap-1 truncate px-4 py-2 hover:bg-background/40"
-          @click="selectOption(option.value)"
+          @click.stop="selectOption(option.value)"
         >
           <Icon v-if="option?.icon" :name="option.icon" />
           <span>{{ option?.label ?? option.value }}</span>
@@ -38,6 +34,8 @@
 </template>
 
 <script setup lang="ts">
+import { onClickOutside } from "@vueuse/core";
+
 const emit = defineEmits<{
   select: [value: string];
 }>();
@@ -53,15 +51,16 @@ defineProps<{
 }>();
 
 const isOpen = ref(false);
+const dropdown = useTemplateRef("dropdown");
 
-const toggleDropdown = (event: Event) => {
-  event.preventDefault();
+onClickOutside(dropdown, () => (isOpen.value = false));
 
+const toggleDropdown = () => {
   isOpen.value = !isOpen.value;
 };
 
 const selectOption = (value: string) => {
-  isOpen.value = false;
   emit("select", value);
+  isOpen.value = false;
 };
 </script>
